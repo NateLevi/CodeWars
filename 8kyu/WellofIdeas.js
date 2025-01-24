20ae6e99d50b0d1cf56c15a04621f62d9c2eb23a
Nate Levi <nathan.levi8@gmail.com>
2025-01-24 15:43:05 -0500

    Create WellofIdeas.js

---
+++ 8kyu/WellofIdeas.js
@@ -0,0 +1,26 @@
+/*
+Description:
+For every good kata idea there seem to be quite a few bad ones!
+In this kata you need to check the provided array (x) for good ideas 'good' and bad ideas
+'bad'. If there are one or two good ideas, return 'Publish!', 
+if there are more than 2 return 'I smell a series!'. If there are no good ideas, 
+as is often the case, return 'Fail!'.
+*/
+//answer
+function well(x){
+  
+  let sum = 0; 
+  
+  x.forEach((element) => {
+    if(element === 'good'){
+    sum++}
+  })
+  
+  if (sum > 2) {
+    return 'I smell a series!';
+  } else if (sum >= 1) {
+    return 'Publish!';
+  } else {
+    return 'Fail!';
+  }
+}
