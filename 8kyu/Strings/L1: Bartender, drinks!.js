acd9d6f8f71eba9f94ff2d13b6489d2d6955672f
Nate Levi <nathan.levi8@gmail.com>
2025-02-16 11:39:41 -0500

    Create L1: Bartender, drinks!.js

---
+++ 8kyu/Strings/L1: Bartender, drinks!.js
@@ -0,0 +1,38 @@
+/*
+Complete the function that receives as input a string, and produces outputs according to the following table:
+
+Input	Output
+"Jabroni"	"Patron Tequila"
+"School Counselor"	"Anything with Alcohol"
+"Programmer"	"Hipster Craft Beer"
+"Bike Gang Member"	"Moonshine"
+"Politician"	"Your tax dollars"
+"Rapper"	"Cristal"
+anything else	"Beer"
+Note: anything else is the default case: if the input to the function is not any of the values in the table, then the return value should be "Beer".
+
+Make sure you cover the cases where certain words do not show up with correct capitalization. For example, the input "pOLitiCIaN" should still return "Your tax dollars".
+*/
+function getDrinkByProfession(param){
+  let lowered = param.toLowerCase();
+  switch (lowered){
+      case 'jabroni':
+        return 'Patron Tequila'
+      case 'school counselor':
+        return 'Anything with Alcohol'
+      case 'programmer':
+        return 'Hipster Craft Beer'
+      case 'bike gang member':
+        return 'Moonshine'
+      case 'politician':
+        return 'Your tax dollars'
+      case 'rapper':
+        return 'Cristal'
+      default:
+        return 'Beer'  }
+}
+/*
+P: a string that could be lower or upper cased
+R: output dependent on the tables input
+E: "Jabroni" -> "Patron Tequila"
+*/
