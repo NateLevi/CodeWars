cae0e5cc7edaebc61895cec82aa6c243176e199f
Nate Levi <nathan.levi8@gmail.com>
2025-02-18 10:02:35 -0500

    Create Alan Partridge II - Apple Turnover.js

---
+++ 8kyu/Strings/Alan Partridge II - Apple Turnover.js
@@ -0,0 +1,28 @@
+/*
+function apple(x){
+if(x**2 >= 1000){
+  return "It's hotter than the sun!!"
+}
+else{
+  return "Help yourself to a honeycomb Yorkie for the glovebox."
+}
+}
+/*
+P: number or string of a number
+R: "It's hotter than the sun!!", else, return Help yourself to a honeycomb Yorkie for the glovebox.
+E: (50) -> "It's hotter than the sun!!"
+*/
+*/
+function apple(x){
+if(x**2 >= 1000){
+  return "It's hotter than the sun!!"
+}
+else{
+  return "Help yourself to a honeycomb Yorkie for the glovebox."
+}
+}
+/*
+P: number or string of a number
+R: "It's hotter than the sun!!", else, return Help yourself to a honeycomb Yorkie for the glovebox.
+E: (50) -> "It's hotter than the sun!!"
+*/
