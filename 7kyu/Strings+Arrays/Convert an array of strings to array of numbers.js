2bd8785f0d7ec654f9e86bd6eb994c722dd71f70
Nate Levi <nathan.levi8@gmail.com>
2025-02-28 10:21:55 -0500

    Create Convert an array of strings to array of numbers.js

---
+++ 7kyu/Strings+Arrays/Convert an array of strings to array of numbers.js
@@ -0,0 +1,8 @@
+function toNumberArray(stringarray){
+  return stringarray.map(element=> Number(element))
+}
+/*
+P: array of numbers as strings
+R: array of those numbers but as numbers not strings
+E: ['1','2','3'] -> [1,2,3]
+*/
