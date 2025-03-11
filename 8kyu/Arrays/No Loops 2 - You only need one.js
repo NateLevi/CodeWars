e8fca4bb357466c338724900d224bba7611b424d
Nate Levi <nathan.levi8@gmail.com>
2025-03-11 08:00:01 -0400

    Update No Loops 2 - You only need one.js

--- 8kyu/Arrays/No Loops 2 - You only need one.js
+++ 8kyu/Arrays/No Loops 2 - You only need one.js
@@ -1,14 +1,11 @@
 /*
-You will be given an array a and a value x. All you need to do is check whether the provided array contains the value, without using a loop.
-
-Array can contain numbers or strings. x can be either. Return true if the array contains the value, false if not. With strings you will need to account for case.
-*/
-function check(a,x){
-  return a.includes(x)
-};
-
-/*
 P: array for a and value for x 
 R: boolean true or false
 E: ([66, 101], 66), true)
 */
+//1
+function check(a,x){
+  return a.includes(x)
+};
+//2
+const check = (a,x)=> a.includes(x)
