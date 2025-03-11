f8f7d306f5404d80561fd2d319b126e26a13e062
Nate Levi <nathan.levi8@gmail.com>
2025-03-11 08:10:30 -0400

    Create Ones and Zeros.js

---
+++ 7kyu/Arrays/Ones and Zeros.js
@@ -0,0 +1,10 @@
+//1
+const binaryArrayToNumber = arr => {
+  const num = arr.join('')
+  return parseInt(num,2)
+};
+/*
+P: array of numbers representing binary
+R: binary value of numbers
+E: [0,0,0,1]
+*/
