1e905744db516b652baffeb9b4c4cd1db5bc8d51
Nate Levi <nathan.levi8@gmail.com>
2025-03-12 12:08:05 -0400

    Update HeadisWrong.js

--- 8kyu/Arrays/HeadisWrong.js
+++ 8kyu/Arrays/HeadisWrong.js
@@ -1,11 +1,9 @@
 /*
-You're at the zoo... all the meerkats look weird. Something has gone terribly wrong - someone has gone and switched their heads and tails around!
-
-Save the animals by switching them back. You will be given an array which will have three values (tail, body, head). It is your job to re-arrange the array so that the animal is the right way round (head, body, tail).
-
-Same goes for all the other arrays/lists that you will get in the tests: you have to change the element positions with the same exact logics
-
-Simples!
+P: array of tail,body,head
+R: same array but head,body,tail
+E; ["tail","body","head"]->["head","body","tail"]
 */
-
+//1
 const fixTheMeerkat = (arr) => arr.reverse()
+//2
+const fixTheMeerkat = arr => [arr[2],arr[1],arr[0]]
