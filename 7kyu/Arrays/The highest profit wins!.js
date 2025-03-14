fa70817cf4e187e713b677fb57adac12b8f419dd
Nate Levi <nathan.levi8@gmail.com>
2025-03-14 08:38:47 -0400

    Create The highest profit wins!.js

---
+++ 7kyu/Arrays/The highest profit wins!.js
@@ -0,0 +1,8 @@
+function minMax(arr){
+  return [Math.min(...arr),Math.max(...arr)];
+}
+/*
+P: array of numbers 
+R: array of the minimum and maximum number of the array
+E: [1,2,3,4,5]-> [1,5]
+*/
