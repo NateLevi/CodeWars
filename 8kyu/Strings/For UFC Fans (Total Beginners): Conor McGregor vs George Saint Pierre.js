d103ede93c627decba50b72fe7fce3861c7ef78e
Nate Levi <nathan.levi8@gmail.com>
2025-06-27 13:40:04 -0400

    Create For UFC Fans (Total Beginners): Conor McGregor vs George Saint Pierre.js

---
+++ 8kyu/Strings/For UFC Fans (Total Beginners): Conor McGregor vs George Saint Pierre.js
@@ -0,0 +1,4 @@
+function quote(fighter) {
+  let name = fighter.toLowerCase()
+  return name === 'george saint pierre' ? "I am not impressed by your performance." : "I'd like to take this chance to apologize.. To absolutely NOBODY!"
+};
