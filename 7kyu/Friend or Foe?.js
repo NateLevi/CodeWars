e95aefcca48437866bf017544ee822efcc9c00c9
Nate Levi <nathan.levi8@gmail.com>
2025-07-01 12:08:20 -0400

    Create Friend or Foe?.js

---
+++ 7kyu/Friend or Foe?.js
@@ -0,0 +1,25 @@
+// --- Codewars Link: https://www.codewars.com/kata/55b42574ff091733d900002f/train/javascript
+// --- Title: Friend or Foe?
+// --- Kyu: 7 kyu
+// --- Date Solved: 7/1/25
+
+/*
+P: list of string names
+R: return list of my friends name
+E: Input = ["Ryan", "Kieran", "Jason", "Yous"] Output = ["Ryan", "Yous"]
+P: Loop through friends, filter out elements with length = 4
+*/
+
+// --- My Solution:
+function friend(friends){
+  return friends.filter((element)=> element.length === 4)
+}
+/*
+--- Reflection & Struggle ---
+
+Primary Struggle Tag: 
+Specific Note: 
+Key Learning/Reinforcement: 
+*/
+
+//Alternative Solution Link: 
