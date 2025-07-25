cb3dc88f45edcb8dca6297f4d8556a78913c56ed
Nate Levi <nathan.levi8@gmail.com>
2025-07-25 18:26:46 -0400

    Create Number of People in the Bus.js

---
+++ 7kyu/Fundamentals/Number of People in the Bus.js
@@ -0,0 +1,10 @@
+var number = function(busStops){
+  let result = 0
+  for(let i=0; i < busStops.length;i++){
+    
+    result += busStops[i][0]
+    result -= busStops[i][1]
+    
+  }
+  return result
+}
