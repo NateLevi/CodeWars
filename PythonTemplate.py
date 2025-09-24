283ac4b59660f7f55a067c2e29df934f556b98a0
Nate Levi <nathan.levi8@gmail.com>
2025-09-24 11:11:20 -0400

    Create PythonTemplate.py

---
+++ PythonTemplate.py
@@ -0,0 +1,24 @@
+# --- Codewars Link: 
+# --- Title: 
+# --- Kyu: 7 kyu
+# --- Date Solved: 
+
+"""
+P: (Parameters) 
+R: (Return) 
+E: (Examples) 
+P: (Pseudocode) 
+"""
+
+# --- My Solution:
+
+
+"""
+--- Reflection & Struggle ---
+
+Primary Struggle Tag: 
+Specific Note: 
+Key Learning/Reinforcement: 
+"""
+
+# --- Alternative Solution Link: 
