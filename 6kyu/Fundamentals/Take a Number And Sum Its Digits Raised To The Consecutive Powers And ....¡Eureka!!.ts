82ec9ed5f585a15ee2337a888b650392160801f8
Nate Levi <nathan.levi8@gmail.com>
2025-10-10 10:59:33 -0400

    Enhance comments and metadata in the Eureka solution
    Added metadata and comments for clarity and context.

---
+++ 6kyu/Fundamentals/Take a Number And Sum Its Digits Raised To The Consecutive Powers And ....¡Eureka!!.ts
@@ -0,0 +1,21 @@
+// --- Codewars Link: https://www.codewars.com/kata/5626b561280a42ecc50000d1/train/javascript
+// --- Title: Take a Number And Sum Its Digits Raised To The Consecutive Powers And ....¡Eureka!!
+// --- Kyu: 6 kyu
+// --- Date Solved: 10/10/25
+
+// a and b are the range of numbers
+
+//loop from a to b, check every number by splitting it, raising each by 1 power, then checking sum, if the sum equals
+// original number then return a list of those numbers
+
+// --- My Solution:
+
+/*
+--- Reflection & Struggle ---
+
+Primary Struggle Tag: small logic issues, like what looping over, curly bracs in map method
+Specific Note: couldn't understand question at first, had to use gpt to break it down simply
+Key Learning/Reinforcement: 
+*/
+
+//Alternative Solution Link: 
